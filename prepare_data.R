cd7f511516b3f0be34c64fc132066c0537d27fda
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-09-23 14:49:07 -0400

    don't source metadata.R
    (copied in read_metadata() function instead)

--- prepare_data.R
+++ prepare_data.R
@@ -1,5 +1,34 @@
-# requires metadata.R: change path as needed
-source("~/Developer/dfr-analysis/metadata.R")
+# copied from github/agoldst/dfr-analysis/metadata.R
+
+read_metadata <- function(filenames,...) {
+
+    read_citations <- function(filename=NA,...) { 
+        f <- filename
+        if(is.na(filename)) { 
+            cat("Select citations.CSV file from jstor dfr...\n")
+            ignore <- readline("(press return to open file dialog) ")
+            f <- file.choose()
+            print(f)
+        }
+
+        # the nefarious trailing comma:
+        cols <- scan(f,nlines=1,what=character(),sep=",",quiet=T)
+        cols <- c(cols,"unused")
+
+        subset(read.csv(f,skip=1,header=F,col.names=cols,quote="",as.is=T,...),
+               select=-unused)
+    }
+    all_rows <- do.call(rbind,lapply(filenames,read_citations,...))
+    # deduplicate
+    result <- unique(all_rows)
+
+    if(any(duplicated(result$id))) {
+        warning("Some rows have the same id")
+    }
+
+    result
+}
+
 
 prepare_data <- function(dfr_dirs,
                          out_dir="data",
