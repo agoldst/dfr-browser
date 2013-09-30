32e51c279e4b2e1a6477039b81ac54e5d0c9a288
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-09-29 21:19:11 -0400

    testing 'framework' + dt_ok()

--- tests.js
+++ tests.js
@@ -1,32 +1,61 @@
 
-dt_ok = function () {
+var dt_ok,
+    yearly_topic_ok;
+
+dt_ok = function (m) {
     // unroll sparse matrix
     // column indices = rep(seq_along(diff(p)),diff(p))
-    my._dt = [];
-    for (j = 0; j < parsed.p.length - 1; j += 1) {
-        for (i = parsed.p[j]; i < parsed.p[j + 1]; i += 1) {
-            row = parsed.i[i];
-            if (!my._dt[row]) {
-                my._dt[row] = [];
-            }
 
-            my._dt[row][j] = parsed.x[i];
-        }
-    }
-    for (d = 0; d < this.n_docs(); d += 1) {
-        for (t = 0; t < this.n(); t += 1) {
-            if (this.dt(d, t) != my._dt[d][t]) { // works if _dt[d][t] undef?
-                return false;
+    load_data("dt.json", function (error, s) {
+        var parsed = JSON.parse(s),
+            dt, j, i, d, t;
+
+        dt = [];
+        for (j = 0; j < parsed.p.length - 1; j += 1) {
+            for (i = parsed.p[j]; i < parsed.p[j + 1]; i += 1) {
+                row = parsed.i[i];
+                if (!dt[row]) {
+                    dt[row] = [];
+                }
+
+                dt[row][j] = parsed.x[i];
             }
         }
-    }
-    return true;
+        for (d = 0; d < m.n_docs(); d += 1) {
+            for (t = 0; t < m.n(); t += 1) {
+                if (dt[d][t] === undefined) {
+                    dt[d][t] = 0;
+                }
+                if (m.dt(d, t) !== dt[d][t]) {
+                    console.log("dt_ok: failed");
+                    console.log("d: " + d + "; t: " + t);
+                    console.log("m.dt(d, t): " + m.dt(d, t));
+                    console.log("dt[d][t]: " + dt[d][t]);
+                    return;
+                }
+            }
+        }
+        console.log("dt_ok: passed");
+    });
 };
             
-yearly_topic = function () {
+/*
+for (n = my.dt.p[t]; n < my.dt.p[t + 1]; n += 1) {
+    if (my.dt.i[n] == d) {
+        return my.dt.x[n];
+    }
+    else if (my.dt.i[n] > d) {
+        return 0;
+    }
+}
+return 0;
+*/
+
+yearly_topic_ok = function () {
     // TODO do it just for a single topic t: faster?? on sparse column
     var counts, totals, result, i, y, t;
 
+    /*
     // cached? 
     if (my.yearly_topic) {
         return my.yearly_topic;
@@ -34,6 +63,7 @@ yearly_topic = function () {
     if (!this.dt() || !this.meta()) {
         return undefined;
     }
+    */
 
     counts = d3.map();
     totals = d3.map();
