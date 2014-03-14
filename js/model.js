87356886b5763f3ba6ea1c25e04be029b942b91d
Andrew Goldstone <andrew.goldstone@gmail.com>
2014-03-14 18:45:27 -0400

    de-linting and commenting model

--- js/model.js
+++ js/model.js
@@ -1,3 +1,5 @@
+/*global d3, utils */
+"use strict";
 var model;
 
 // model specification
@@ -11,18 +13,35 @@ var model;
 model = function (spec) {
     var my = spec || { }, // private members
         that = { }, // resultant object
-        // methods
-        info, dt, n_docs, doc_len, tw, n, n_top_words, alpha, meta, vocab,
+        info, // accessors and pseudo-accessors
+        dt,
+        n_docs,
+        doc_len,
+        tw,
+        n,
+        n_top_words,
+        alpha,
+        meta,
+        vocab,
         topic_scaled,
-        yearly_topic, topic_yearly, doc_year, yearly_total,
-        topic_docs, topic_words, doc_topics, word_topics,
-        set_dt, set_tw, set_meta, set_doc_len, set_topic_scaled;
+        topic_yearly, // time-slicing
+        doc_year,
+        yearly_total,
+        topic_docs, // most salient _ in _
+        topic_words,
+        doc_topics,
+        word_topics,
+        set_dt, // methods for loading model data from strings 
+        set_tw,
+        set_meta,
+        set_doc_len,
+        set_topic_scaled;
 
 
     info = function (model_meta) {
         if (model_meta) {
             my.info = model_meta;
-        } 
+        }
         return my.info;
     };
     that.info = info;
@@ -31,25 +50,30 @@ model = function (spec) {
         var p0, p;
         if (!my.dt) {
             return undefined;
-        } else if (d === undefined ) {
+        }
+
+        // dt() for the whole matrix
+        if (d === undefined) {
             return my.dt;
-        } else if (t === undefined) {
+        }
+
+        // dt(d) for a whole document row
+        if (t === undefined) {
             return d3.range(this.n()).map(function (j) {
                 return this.dt(d, j);
             });
-        } else {
-            p0 = my.dt.p[t];
-            p = d3.bisectLeft(my.dt.i.slice(p0, my.dt.p[t + 1]),d);
-            if (my.dt.i[p + p0] === d) {
-                return my.dt.x[p + p0];
-            } else {
-                return 0;
-            }
-
         }
+
+        // dt(d, t) for one entry
+        p0 = my.dt.p[t];
+        p = d3.bisectLeft(my.dt.i.slice(p0, my.dt.p[t + 1]), d);
+
+        // if there is no d entry for column t, return 0
+        return (my.dt.i[p + p0] === d) ? my.dt.x[p + p0] : 0;
     };
+    // a row_sum method for the dt object
     dt.row_sum = function (d) {
-        var result, t, n;
+        var result, t;
         if (!my.dt) {
             return undefined;
         }
@@ -80,89 +104,110 @@ model = function (spec) {
             result += 1;
         }
         my.n_docs = result;
-    
         return result; // undefined if both my.meta and my.dt are missing
     };
     that.n_docs = n_docs;
 
+    // though we could take sums of rows of dt, that would give us
+    // token counts rather than word counts. Instead, we expect
+    // to be given these precalculated
     doc_len = function (d) {
         if (!my.doc_len) {
             return undefined;
             //my.doc_len = [];
-        } else if (d === undefined) {
-            return my.doc_len;
-        } else {
-            /*
-            if(!my.doc_len[d]) {
-                my.doc_len[d] = this.dt.row_sum(d);
-            }
-            */
-            return my.doc_len[d];
         }
+
+        // doc_len() for the whole list
+        if (d === undefined) {
+            return my.doc_len;
+        }
+
+        return my.doc_len[d];
     };
     that.doc_len = doc_len;
 
+    // access top key words per topic
     tw = function (t, word) {
         if (!my.tw) {
             return undefined;
-        } else if (t === undefined) {
-            return my.tw;
-        } else if (word === undefined) {
-            return my.tw[t];
-        } else {
-            return my.tw[t].get(word);
         }
+
+        // tw() for the whole list of hashes
+        if (t === undefined) {
+            return my.tw;
+        }
+
+        // tw(t) for a particular topic
+        if (word === undefined) {
+            return my.tw[t];
+        }
+
+        // tw(t, word) for the weight of word in topic t
+        return my.tw[t].get(word);
     };
     that.tw = tw;
 
+    // number of topics
     n = function () {
         return my.n;
     };
     that.n = n;
 
+    // number of top words per topic stored in tw
     n_top_words = function () {
         if (!this.tw()) {
             return undefined;
-        } else {
-            return my.tw[0].keys().length;
         }
+
+        return my.tw[0].keys().length;
     };
     that.n_top_words = n_top_words;
 
+    // alpha hyperparameters
     alpha = function (t) {
         if (!my.alpha) {
             return undefined;
         }
+
+        // alpha() for all of them or alpha(t) for just one
         return isFinite(t) ? my.alpha[t] : my.alpha;
     };
     that.alpha = alpha;
 
+    // metadata table
     meta = function (d) {
         if (!my.meta) {
             return undefined;
         }
+
+        // meta(d) for one row of doc metadata or meta() for all of them
         return isFinite(d) ? my.meta[d] : my.meta;
     };
     that.meta = meta;
-    
+
+    // convenience function for getting doc dates as years
     doc_year = function (d) {
         if (!my.meta) {
             return undefined;
         }
+
+        // memoization
         if (!my.doc_year) {
             my.doc_year = [];
         }
         if (my.doc_year[d] !== undefined) {
             return my.doc_year[d];
-        } else {
-            my.doc_year[d] = meta(d).date.getFullYear();
-            return my.doc_year[d];
         }
+
+        my.doc_year[d] = meta(d).date.getFullYear();
+        return my.doc_year[d];
     };
     that.doc_year = doc_year;
 
+    // aggregate vocabulary of all top words in tw
     vocab = function () {
         var result;
+        // memoization
         if (my.vocab) {
             return my.vocab;
         }
@@ -181,40 +226,47 @@ model = function (spec) {
     };
     that.vocab = vocab;
 
+    // scaled coordinates for topics
     topic_scaled = function (t) {
         if (!my.topic_scaled) {
             return undefined;
-        } else if (t === undefined) {
-            return my.topic_scaled;
-        } else {
-            return my.topic_scaled[t];
         }
+
+        // topic_scaled() for all of them
+        if (t === undefined) {
+            return my.topic_scaled;
+        }
+
+        // topic_scaled(t) for coordinates for topic t
+        return my.topic_scaled[t];
     };
     that.topic_scaled = topic_scaled;
 
+    // get aggregate topic counts over years
     yearly_total = function (y) {
         var result;
+        // memoization
         if (my.yearly_total) {
             return my.yearly_total.get(y);
-        } else {
-            result = d3.map();
-            for (n = 0; n < my.dt.i.length; n += 1) {
-                y = this.doc_year(my.dt.i[n]);
-                if (result.has(y)) {
-                    result.set(y, result.get(y) + my.dt.x[n]);
-                }
-                else {
-                    result.set(y, my.dt.x[n]);
-                }
-            }
-            my.yearly_total = result;
-            return result.get(y);
         }
-    }; 
+
+        result = d3.map();
+        for (n = 0; n < my.dt.i.length; n += 1) {
+            y = this.doc_year(my.dt.i[n]);
+            if (result.has(y)) {
+                result.set(y, result.get(y) + my.dt.x[n]);
+            } else {
+                result.set(y, my.dt.x[n]);
+            }
+        }
+        my.yearly_total = result;
+        return result.get(y);
+    };
     that.yearly_total = yearly_total;
 
+    // yearly proportions for topic t
     topic_yearly = function (t) {
-        var result, n, y;
+        var result, j, y;
 
         // cached? 
         if (!my.topic_yearly) {
@@ -228,17 +280,19 @@ model = function (spec) {
 
         result = d3.map();
 
-        for (n = my.dt.p[t]; n < my.dt.p[t + 1]; n += 1) {
-            y = this.doc_year(my.dt.i[n]);
+        for (j = my.dt.p[t]; j < my.dt.p[t + 1]; j += 1) {
+            y = this.doc_year(my.dt.i[j]);
             if (result.has(y)) {
-                result.set(y, result.get(y) + my.dt.x[n]);
+                result.set(y, result.get(y) + my.dt.x[j]);
             } else {
-                result.set(y, my.dt.x[n]);
+                result.set(y, my.dt.x[j]);
             }
         }
 
         // divide through
         result.forEach(function (y, w) {
+            // have to use "that" and not "this" because "this" changes
+            // inside forEach
             result.set(y, w / that.yearly_total(y));
         });
 
@@ -248,23 +302,28 @@ model = function (spec) {
     };
     that.topic_yearly = topic_yearly;
 
+    // Get n top documents for topic t. Uses a naive document ranking,
+    // by the proportion of words assigned to t, which does *not*
+    // necessarily give the docs where t is most salient
     topic_docs = function (t, n) {
         var p0 = my.dt.p[t],
             p1 = my.dt.p[t + 1],
-            m = this,
-            docs, bisect, insert, i, result = [];
-        // naive document ranking, by the proportion of words assigned to t,
-        // which does *not* necessarily give the docs where t is most salient
+            docs,
+            bisect,
+            insert,
+            i,
+            result = [];
 
         // column slice
         docs = d3.range(p0, p1).map(function (p) {
             return {
                 doc: my.dt.i[p],
-                frac: my.dt.x[p] / m.doc_len(my.dt.i[p]),
+                frac: my.dt.x[p] / that.doc_len(my.dt.i[p]),
                 weight: my.dt.x[p]
             };
         });
 
+        // return them all, sorted, if there are fewer than n hits
         if (n >= docs.length) {
             docs.sort(function (a, b) {
                 return d3.descending(a.frac, b.frac);
@@ -298,17 +357,18 @@ model = function (spec) {
     };
     that.topic_docs = topic_docs;
 
-    // unlike with docs, no reason to go to lengths to avoid sorting n_topics
-    // entries, since n_topics << n_docs. The expensive step is the row slice, 
-    // which we have to do anyway.
+    // Get n top topics for document d. Unlike with docs, no reason to
+    // go to lengths to avoid sorting n_topics entries, since
+    // n_topics << n_docs. The expensive step is the row slice, which we
+    // have to do anyway.
     doc_topics = function (d, n) {
-        var m = this,
-            topics, i;
+        var topics;
+
         topics = d3.range(my.n)
             .map(function (t) {
                 return {
                     topic: t,
-                    weight: m.dt(d, t)
+                    weight: that.dt(d, t)
                 };
             })
             .filter(function (d) {
@@ -325,20 +385,22 @@ model = function (spec) {
     };
     that.doc_topics = doc_topics;
 
+    // Get n top words for topic t.
     topic_words = function (t, n) {
         var n_words = n || this.n_top_words(),
-            m = this,
             words = this.tw(t).entries(); // d3.map method
         words.sort(function (w1, w2) {
             return d3.descending(w1.value, w2.value);
         });
+
         return utils.shorten(words, n_words, function (ws, i) {
             return ws[i].value;
         })
-        .map(function (w) { return w.key; });
+            .map(function (w) { return w.key; });
     };
     that.topic_words = topic_words;
 
+    // Get n top topics for a word.
     word_topics = function (word, n) {
         var t, row, word_wt,
             n_topics = n || this.n_top_words(),
@@ -370,10 +432,11 @@ model = function (spec) {
     };
     that.word_topics = word_topics;
 
+    // load tw from a string of JSON
     set_tw = function (tw_s) {
         var parsed;
 
-        if (typeof(tw_s) !== 'string') {
+        if (typeof tw_s !== 'string') {
             return;
         }
 
@@ -382,7 +445,7 @@ model = function (spec) {
         my.tw = parsed.tw.map(function (topic) {
             var result = d3.map();
             topic.words.map(function (w, j) {
-                result.set(w,topic.weights[j]);
+                result.set(w, topic.weights[j]);
             });
             return result;
         });
@@ -393,8 +456,9 @@ model = function (spec) {
     };
     that.set_tw = set_tw;
 
+    // load dt from a string of JSON
     set_dt = function (dt_s) {
-        if (typeof(dt_s) !== 'string') {
+        if (typeof dt_s  !== 'string') {
             return;
         }
         my.dt = JSON.parse(dt_s);
@@ -404,15 +468,16 @@ model = function (spec) {
     };
     that.set_dt = set_dt;
 
+    // load meta from a string of CSV lines
     set_meta = function (meta_s) {
         var s;
-        if (typeof(meta_s) !== 'string') {
+        if (typeof meta_s !== 'string') {
             return;
         }
 
         // strip blank "rows" at start or end
-        s = meta_s.replace(/^\n*/,"")
-            .replace(/\n*$/,"\n");
+        s = meta_s.replace(/^\n*/, "")
+            .replace(/\n*$/, "\n");
 
         my.meta = d3.csv.parseRows(s, function (d, j) {
         // no header, but this is the column order:
@@ -437,23 +502,25 @@ model = function (spec) {
     };
     that.set_meta = set_meta;
 
+    // load doc lengths from a string of JSON
     set_doc_len = function (s) {
-        if (typeof(s) !== 'string') {
+        if (typeof s  !== 'string') {
             return;
         }
         my.doc_len = JSON.parse(s).doc_len;
     };
     that.set_doc_len = set_doc_len;
 
-    set_topic_scaled = function(ts_s) {
+    // load scaled topic coordinates from a string of CSV lines
+    set_topic_scaled = function (ts_s) {
         var s;
-        if (typeof(ts_s) !== 'string') {
+        if (typeof ts_s  !== 'string') {
             return;
         }
 
         // strip blank "rows" at start or end
-        s = ts_s.replace(/^\n*/,"")
-            .replace(/\n*$/,"\n");
+        s = ts_s.replace(/^\n*/, "")
+            .replace(/\n*$/, "\n");
         my.topic_scaled = d3.csv.parseRows(s, function (row) {
             return row.map(parseFloat);
         });
