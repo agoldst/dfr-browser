8b54b1537f9e3c288be0036ad9bfba1edb8d4c9d
Andrew Goldstone <andrew.goldstone@gmail.com>
2014-05-27 10:20:22 -0400

    move doc_topics_matrix into worker
    because the worker is now the only one that uses it

--- src/worker.js
+++ src/worker.js
@@ -1,60 +1,101 @@
 /*global importScripts, doc_topics_matrix, utils, onmessage, postMessage */
 "use strict";
-importScripts("dt.js", "utils.js");
+importScripts("utils.min.js");
 
 var my = { },
+    doc_topics_matrix,
     total_tokens,
     topic_docs,
     doc_topics,
     topic_yearly,
     yearly_total;
 
-onmessage = function (e) {
-    if (e.data.what === "set_dt") {
-        my.dt = doc_topics_matrix(e.data.dt);
-        postMessage({
-            what: "set_dt",
-            result: my.dt !== undefined
-        });
-    } else if (e.data.what === "set_doc_years") {
-        my.doc_years = e.data.doc_years;
-        postMessage({
-            what: "set_doc_years",
-            result: my.doc_years !== undefined
-        });
-    } else if (e.data.what === "total_tokens") {
-        postMessage({
-            what: "total_tokens",
-            result: total_tokens()
-        });
-    } else if (e.data.what === "topic_docs") {
-        postMessage({
-            what: "topic_docs/" + e.data.t + "/" + e.data.n,
-            result: topic_docs(e.data.t, e.data.n)
-        });
-    } else if (e.data.what === "doc_topics") {
-        postMessage({
-            what: "doc_topics/" + e.data.d + "/" + e.data.n,
-            result: doc_topics(e.data.d, e.data.n)
-        });
-    } else if (e.data.what === "topic_total") {
-        postMessage({
-            what: "topic_total/" + e.data.t,
-            result: my.dt.col_sum(e.data.t === "all" ? undefined : e.data.t)
-        });
-    } else if (e.data.what === "topic_yearly") {
-        postMessage({
-            what: "topic_yearly/" + e.data.t,
-            result: topic_yearly(e.data.t === "all" ? undefined : e.data.t)
-        });
-    } else if (e.data.what === "yearly_total") {
-        postMessage({
-            what: "yearly_total/" + e.data.y,
-            result: yearly_total(e.data.y === "all" ? undefined : e.data.y)
-        });
-    } else {
-        postMessage({ what: "error" });
+doc_topics_matrix = function (data) { 
+    var my = { },
+        that = data;
+
+    if (that.p && that.p.length) {
+        that.n = that.p.length - 1;
     }
+
+    that.get = function (d, t) {
+        var p0, p, j, result;
+        if (!this.x) {
+            return undefined;
+        }
+
+        // dt() for the whole matrix
+        if (d === undefined) {
+            return this;
+        }
+
+        // dt(d) for a whole document row
+        if (t === undefined) {
+            result = [ ];
+            for (j = 0; j < this.n; j += 1) {
+                result.push(this.get(d, j));
+            }
+            return result;
+        }
+
+        // dt(d, t) for one entry
+        p0 = this.p[t];
+        p = utils.bisect_left(this.i.slice(p0, this.p[t + 1]), d);
+
+        // if there is no d entry for column t, return 0
+        result = (this.i[p + p0] === d) ? this.x[p + p0] : 0;
+        return result;
+    };
+
+    // a naive row_sum method for the dt object
+    that.row_sum = function (d) {
+        var result, t;
+        if (!this.x) {
+            return undefined;
+        }
+
+        // memoize, at least
+        if (!my.row_sum) {
+            my.row_sum = [ ];
+        }
+
+        if (!my.row_sum[d]) {
+            result = 0;
+            for (t = 0; t < this.n; t += 1) {
+                result += this.get(d, t);
+            }
+            my.row_sum[d] = result;
+        }
+        return my.row_sum[d];
+    };
+    // a col_sum method: this takes advantages of the column compression
+    that.col_sum = function (t) {
+        var i;
+
+        // memoization
+        if (!my.col_sum) {
+            my.col_sum = [];
+        }
+
+        // dt.col_sum() returns an array of sums
+        if (t === undefined) {
+            for (i = 0; i <= this.n; i += 1) {
+                this.col_sum(i); // stores the result
+            }
+            return my.col_sum;
+        }
+
+        // otherwise, return the sum for column t
+        if (!my.col_sum[t]) {
+            my.col_sum[t] = 0;
+            for (i = this.p[t]; i < this.p[t + 1]; i += 1) {
+                my.col_sum[t] += this.x[i];
+            }
+        }
+        return my.col_sum[t];
+    };
+
+    return that;
 };
 
 total_tokens = function () {
@@ -206,4 +247,53 @@ yearly_total = function (year) {
     }
 
     return isFinite(year) ? my.yearly_total[year] : my.yearly_total;
+};
+
+// main dispatch
+onmessage = function (e) {
+    if (e.data.what === "set_dt") {
+        my.dt = doc_topics_matrix(e.data.dt);
+        postMessage({
+            what: "set_dt",
+            result: my.dt !== undefined
+        });
+    } else if (e.data.what === "set_doc_years") {
+        my.doc_years = e.data.doc_years;
+        postMessage({
+            what: "set_doc_years",
+            result: my.doc_years !== undefined
+        });
+    } else if (e.data.what === "total_tokens") {
+        postMessage({
+            what: "total_tokens",
+            result: total_tokens()
+        });
+    } else if (e.data.what === "topic_docs") {
+        postMessage({
+            what: "topic_docs/" + e.data.t + "/" + e.data.n,
+            result: topic_docs(e.data.t, e.data.n)
+        });
+    } else if (e.data.what === "doc_topics") {
+        postMessage({
+            what: "doc_topics/" + e.data.d + "/" + e.data.n,
+            result: doc_topics(e.data.d, e.data.n)
+        });
+    } else if (e.data.what === "topic_total") {
+        postMessage({
+            what: "topic_total/" + e.data.t,
+            result: my.dt.col_sum(e.data.t === "all" ? undefined : e.data.t)
+        });
+    } else if (e.data.what === "topic_yearly") {
+        postMessage({
+            what: "topic_yearly/" + e.data.t,
+            result: topic_yearly(e.data.t === "all" ? undefined : e.data.t)
+        });
+    } else if (e.data.what === "yearly_total") {
+        postMessage({
+            what: "yearly_total/" + e.data.y,
+            result: yearly_total(e.data.y === "all" ? undefined : e.data.y)
+        });
+    } else {
+        postMessage({ what: "error" });
+    }
 };
