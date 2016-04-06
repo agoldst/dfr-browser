c2e4beb9b3491e773baa9c79975e07e10f2bd276
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-04-06 14:33:54 -0400

    some reshuffling, including: browser -> dfb
    main.js now goes by the less offensive dfb.js
    
    the big parameter initialization dealie belongs in its own file: VIS.js.
    This is modified by a new "member" function, VIS.update() -- just the
    once after loading info.json. In general this big baggy global variable
    is pretty depressing and ripe refactoring itself, but for now, let's
    leave it.

---
+++ src/VIS.js
@@ -0,0 +1,115 @@
+/*global d3, utils */
+"use strict";
+
+// declaration of global object (modified by browser().load())
+var VIS = {
+    ready: { }, // which viz already generated?
+    last: { // which subviews last shown?
+        bib: { }
+    },
+    files: { // what data files to request
+        info: "data/info.json",
+        meta: "data/meta.csv.zip",  // remove .zip to use uncompressed data
+        dt: "data/dt.json.zip",     // (name the actual file accordingly)
+        tw: "data/tw.json",
+        topic_scaled: "data/topic_scaled.csv"
+    },
+    default_view: "/model", // specify the part after the #
+    overview_words: 15,     // may need adjustment
+    model_view: {
+        w: 500,            // px: the minimum svg width
+        aspect: 1.3333,     // for calculating height
+        words: 4,           // maximum: may need adjustment
+        size_range: [7, 18], // points. may need adjustment
+        name_size: 18,      // points
+        stroke_range: 6,    // max. perimeter thickness
+        yearly: {
+            w: 500,         // px: the minimum svg width
+            aspect: 1.333,
+            m: {
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 20
+            },
+            label_threshold: 40, // px
+            words: 4,
+            label_words: 2 // should be <= words
+        },
+        list: {
+            spark: {
+                w: 70,
+                h: 20,
+                m: {
+                    left: 2,
+                    right: 2,
+                    top: 2,
+                    bottom: 2
+                },
+                bar_width: 300
+            }
+        }
+    },
+    topic_view: {
+        words: 50,
+        docs: 20,
+        w: 400, // minimum in px
+        aspect: 3,
+        m: {
+            left: 40,
+            right: 20,
+            top: 20,
+            bottom: 20
+        },
+        bar_width: 90, // in days!
+        ticks: 10 // applied to both x and y axes
+    },
+    word_view: {
+        n_min: 10, // words per topic
+        topic_label_padding: 8, // pt
+        topic_label_leading: 14, // pt
+        row_height: 80, // pt
+        svg_rows: 10, // * row_height gives min. height for svg element
+        w: 700, // px: minimum width
+        m: {
+            left: 100,
+            right: 40,
+            top: 20,
+            bottom: 0
+        }
+    },
+    bib: {
+        // default if no author delimiter supplied, but note that
+        // 2014 JSTOR metadata format uses ", " instead
+        author_delimiter: "\t",
+        // "et al" is better for real bibliography, but it's
+        // actually worth being able to search all the multiple authors
+        et_al: Infinity,
+        anon: "[Anon]",
+    },
+    bib_view: {
+        window_lines: 100,
+        major: "year",
+        minor: "authortitle",
+        dir: "up"
+    },
+    float_format: function (x) {
+        return d3.round(x, 3);
+    },
+    tooltip: {              // tooltip div parameters
+        offset: {
+            x: 10,          // px
+            y: 0
+        }
+    },
+    percent_format: d3.format(".1%"),
+    cite_date_format: d3.time.format.utc("%B %Y"), // JSTOR supplies UTC dates
+    resize_refresh_delay: 100, // ms
+    hidden_topics: [],      // list of 1-based topic numbers to suppress
+    show_hidden_topics: false,
+    annotes: [],            // list of CSS classes annotating the current view
+    update: function (x) {
+        return utils.deep_replace(this, x);
+    }
+};
+
--- src/dfb.js
+++ src/dfb.js
@@ -1,121 +1,10 @@
-/*global d3, $, JSZip, model, utils, view, bib, metadata, window, document */
+/*global d3, $, JSZip, model, view, bib, metadata, VIS, window */
 "use strict";
-
-// declaration of global object (modified by load())
-var VIS = {
-    ready: { }, // which viz already generated?
-    last: { // which subviews last shown?
-        bib: { }
-    },
-    files: { // what data files to request
-        info: "data/info.json",
-        meta: "data/meta.csv.zip",  // remove .zip to use uncompressed data
-        dt: "data/dt.json.zip",     // (name the actual file accordingly)
-        tw: "data/tw.json",
-        topic_scaled: "data/topic_scaled.csv"
-    },
-    default_view: "/model", // specify the part after the #
-    overview_words: 15,     // may need adjustment
-    model_view: {
-        w: 500,            // px: the minimum svg width
-        aspect: 1.3333,     // for calculating height
-        words: 4,           // maximum: may need adjustment
-        size_range: [7, 18], // points. may need adjustment
-        name_size: 18,      // points
-        stroke_range: 6,    // max. perimeter thickness
-        yearly: {
-            w: 500,         // px: the minimum svg width
-            aspect: 1.333,
-            m: {
-                left: 20,
-                right: 20,
-                top: 20,
-                bottom: 20
-            },
-            label_threshold: 40, // px
-            words: 4,
-            label_words: 2 // should be <= words
-        },
-        list: {
-            spark: {
-                w: 70,
-                h: 20,
-                m: {
-                    left: 2,
-                    right: 2,
-                    top: 2,
-                    bottom: 2
-                },
-                bar_width: 300
-            }
-        }
-    },
-    topic_view: {
-        words: 50,
-        docs: 20,
-        w: 400, // minimum in px
-        aspect: 3,
-        m: {
-            left: 40,
-            right: 20,
-            top: 20,
-            bottom: 20
-        },
-        bar_width: 90, // in days!
-        ticks: 10 // applied to both x and y axes
-    },
-    word_view: {
-        n_min: 10, // words per topic
-        topic_label_padding: 8, // pt
-        topic_label_leading: 14, // pt
-        row_height: 80, // pt
-        svg_rows: 10, // * row_height gives min. height for svg element
-        w: 700, // px: minimum width
-        m: {
-            left: 100,
-            right: 40,
-            top: 20,
-            bottom: 0
-        }
-    },
-    bib: {
-        // default if no author delimiter supplied, but note that
-        // 2014 JSTOR metadata format uses ", " instead
-        author_delimiter: "\t",
-        // "et al" is better for real bibliography, but it's
-        // actually worth being able to search all the multiple authors
-        et_al: Infinity,
-        anon: "[Anon]",
-    },
-    bib_view: {
-        window_lines: 100,
-        major: "year",
-        minor: "authortitle",
-        dir: "up"
-    },
-    float_format: function (x) {
-        return d3.round(x, 3);
-    },
-    tooltip: {              // tooltip div parameters
-        offset: {
-            x: 10,          // px
-            y: 0
-        }
-    },
-    percent_format: d3.format(".1%"),
-    cite_date_format: d3.time.format.utc("%B %Y"), // JSTOR supplies UTC dates
-    resize_refresh_delay: 100, // ms
-    hidden_topics: [],      // list of 1-based topic numbers to suppress
-    show_hidden_topics: false,
-    annotes: []             // list of CSS classes annotating the current view
-};
-
-/* declaration of functions */
 
 var topic_link, // stringifiers
     topic_hash,
     set_view,   // used by the views: see src/view/*
-    browser;    // main controller
+    dfb;    // main controller
 
 topic_link = function (t) {
     return "#" + topic_hash(t);
@@ -129,7 +18,7 @@ set_view = function (hash) {
     window.location.hash = hash;
 };
 
-browser = function (spec) {
+dfb = function (spec) {
     var my = spec || { },
         that = { },
         topic_view, // view generation
@@ -843,7 +732,7 @@ load = function () {
             // finish initializing VIS by loading any preferences
             // stashed in model info
 
-            VIS = utils.deep_replace(VIS, my.m.info().VIS);
+            VIS.update(my.m.info().VIS);
 
             // now we store any specified options for bib
             my.bib.options(VIS.bib);
@@ -928,9 +817,9 @@ load = function () {
 that.load = load;
 
     return that;
-}; // browser()
+}; // dfb()
 
 // execution is up to index.html:
-// browser({ ... })
+// dfb({ ... })
 //     .load();
 
