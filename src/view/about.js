dd19775b5dcb03754d2a57ebe944de452f2563c2
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-06-14 15:07:20 -0400

    caching the about page: dumb

--- src/view/about.js
+++ src/view/about.js
@@ -2,12 +2,9 @@
 "use strict";
 
 view.about = function (info) {
-    if (view.dirty("about")) {
-        if (info.meta_info) {
-            d3.select("div#meta_info")
-                .html(info.meta_info);
-        }
-        view.dirty("about", false);
+    if (info.meta_info) {
+        d3.select("div#meta_info")
+            .html(info.meta_info);
     }
     return true;
 };
